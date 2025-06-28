5a0d2381a732f62516e4406a26d8fdabe981e4a1
isaac <isdiemer@umich.edu>
2025-06-28 18:13:06 -0400

    fix4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: process.env['BACKEND_URL'] || 'http://localhost:8080',
+    apiUrl: 'http://localhost:8080',
     openaiApiKey: ''
 }; 
